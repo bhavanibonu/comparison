c726f450eac0af01f4285a712a3bf6f0eb780cb4
bhavanibonu <bhavanibonubhavani@gmail.com>
2023-08-24 15:54:06 +0530

    origin

---
+++ Fibonacci sequence.py
@@ -0,0 +1,10 @@
+import time
+start = time.time()
+def fibonacci(n):
+    f=[0, 1 ]
+    for i in range(2, n+1):
+        f.append(f[i-1] + f[i-2])
+    return f[n]
+print(fibonacci(9))
+end = time.time()
+print(f"Runtime of the program is{end - start}")
